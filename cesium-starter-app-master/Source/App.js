e77f03faa68224692df6ded3620405ac67221a09
Toshikazu SETO <tosseto@gmail.com>
2015-10-09 11:14:31 +0900

    add points

--- cesium-starter-app-master/Source/App.js
+++ cesium-starter-app-master/Source/App.js
@@ -1,1 +1,13 @@
+//ビューアーを呼び出す
 var viewer = new Cesium.Viewer('cesiumContainer');
+
+//ポイントを追加
+var point = viewer.entities;
+
+point.add({
+        position : Cesium.Cartesian3.fromDegrees(-75.59777, 40.03883),
+        point : {
+            pixelSize : 10,
+            color : Cesium.Color.YELLOW
+    }
+});
